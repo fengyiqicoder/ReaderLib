cc0c4cc1c5b2e9815bd8f1824af092bb05d64136
FengYQ <329650437@qq.com>
2022-03-03 01:34:42 +0800

    remove zip achieve

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 
-        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.4.2"),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: "4.0.0"),
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.6.1"),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "5.2.0"),
@@ -28,7 +27,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ReaderLib",
-            dependencies: ["AEXML", "ZipArchive", "MenuItemKit","FontBlaster", .product(name: "RealmSwift", package: "Realm"), "SWCompression"],
+            dependencies: ["AEXML", "MenuItemKit","FontBlaster", .product(name: "RealmSwift", package: "Realm"), "SWCompression"],
             resources: [ .process("Resources") ]),
         .testTarget(
             name: "ReaderLibTests",
